ac3347a340e4e8b1d4c2985b68c9d8212e7f0b6e
Oleg <o.selivancev@gmail.com>
2018-12-31 16:39:13 +0300

    reduxApp update

---
+++ app/helper.js
@@ -0,0 +1,5 @@
+// https://tuhub.ru/posts/redux-i-thunk-vmeste-react-rukovodstvo-dlya-chajnikov
+
+// http://jsfiddle.net/cdagli/b2uq8704/6/
+
+// https://github.com/rajdee/redux-in-russian/blob/master/docs/advanced/AsyncActions.md
